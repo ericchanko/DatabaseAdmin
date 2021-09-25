695e90642ba1fadffb0a4f7b408542b5af1b36c5
ericchanko <ericchanko@gmail.com>
2021-09-24 19:55:21 -0700

    starting a1

---
+++ assignment1/main.py
@@ -0,0 +1,18 @@
+
+
+
+def prompt():
+
+    hostname = input("Please enter the MySQL hostname: ")
+    port = input("Please enter the MySQL port: ")
+    username = input("Please enter your MySQL username: ")
+    password = input("Please enter your MySQL password: ")
+
+
+
+def main():
+    prompt()
+
+
+if __name__ == "__main__":
+    main()
